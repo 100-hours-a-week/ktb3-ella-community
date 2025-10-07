2a1a6a9ab8429daabb8c51272e48c05b3649de84
ella <lydbsdud@gmail.com>
2025-10-07 23:17:19 +0900

    first commit

---
+++ src/main/java/com/example/ktb3community/Ktb3CommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.ktb3community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ktb3CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ktb3CommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ktb3community/Ktb3CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktb3community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ktb3CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
