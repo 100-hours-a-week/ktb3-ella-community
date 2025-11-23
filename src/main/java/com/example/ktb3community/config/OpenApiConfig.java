a0d6ef3298b91d679f8f6172b139b7d61ff7e59a
ella <lydbsdud@gmail.com>
2025-11-23 16:39:49 +0900

    feat: swagger header 추가

--- src/main/java/com/example/ktb3community/config/OpenApiConfig.java
+++ src/main/java/com/example/ktb3community/config/OpenApiConfig.java
@@ -2,6 +2,9 @@ package com.example.ktb3community.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +12,20 @@ import org.springframework.context.annotation.Configuration;
 class OpenApiConfig {
     @Bean
     OpenAPI openApi() {
-        return new OpenAPI().info(new Info()
-                .title("KTB3 ella Community API")
-                .version("v1")
-                .description("ella Community API 문서"));
+        String bearerSchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("KTB3 ella Community API")
+                        .version("v1")
+                        .description("ella Community API 문서"))
+                .components(new Components()
+                        .addSecuritySchemes(bearerSchemeName,
+                                new SecurityScheme()
+                                        .name(bearerSchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(bearerSchemeName));
     }
 }
